36d226045d06fa32c93d269f79bd25e0bc3bab17
Alexis Hope <alexis@cvofhope.com>
2017-07-18 11:31:27 +1000

    remove puts from routes files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
-puts '-------------------------- ExampleContent routes'
 ExampleContent::Engine.routes.draw do
-  puts '-------------------------- ExampleContent drawing routes'
   get 'example-content', to: 'content#index'
 end
