95c5d2a7d5dc745f5d5cbd940a2291f30ee741a1
Alexis Hope <alexis@cvofhope.com>
2017-07-18 11:40:23 +1000

    add typography page

--- app/controllers/example_content/content_controller.rb
+++ app/controllers/example_content/content_controller.rb
@@ -1,5 +1,6 @@
 module ExampleContent
   class ContentController < ApplicationController
     def index; end
+    def typography; end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 ExampleContent::Engine.routes.draw do
   get 'example-content', to: 'content#index'
+  get 'example-content/typography', to: 'content#typography'
 end
