98828fd576b2483c7a91d67477268a98042cde66
Alexis Hope <alexis@cvofhope.com>
2017-07-21 10:28:06 +1000

    add bootstrap grid example

--- app/controllers/example_content/content_controller.rb
+++ app/controllers/example_content/content_controller.rb
@@ -2,5 +2,6 @@ module ExampleContent
   class ContentController < ApplicationController
     def index; end
     def typography; end
+    def bootstrap_grid; end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ ExampleContent::Engine.routes.draw do
   if !Rails.env.production?
     get 'example-content', to: 'content#index'
     get 'example-content/typography', to: 'content#typography'
+    get 'example-content/bootstrap-grid', to: 'content#bootstrap_grid'
   end
 end
